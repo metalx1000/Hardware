40fb8cb0beb841be8033a8b5c016a49615217573
metalx1000 <git@filmsbykris.com>
2016-07-12 19:34:20 -0400

    Cloned CaptivePortal for ESP8266

---
+++ ESP8266/CaptivePortal/CaptivePortal.ino
@@ -0,0 +1,45 @@
+/*
+   Captive Portal by: M. Ray Burnette 20150831
+   See Notes tab for original code references and compile requirements
+   Sketch uses 300,640 bytes (69%) of program storage space. Maximum is 434,160 bytes.
+   Global variables use 50,732 bytes (61%) of dynamic memory, leaving 31,336 bytes for local variables. Maximum is 81,920 bytes.
+*/
+
+#include <ESP8266WiFi.h>
+#include "./DNSServer.h"                  // Patched lib
+#include <ESP8266WebServer.h>
+
+const byte        DNS_PORT = 53;          // Capture DNS requests on port 53
+IPAddress         apIP(10, 10, 10, 1);    // Private network for server
+DNSServer         dnsServer;              // Create the DNS object
+ESP8266WebServer  webServer(80);          // HTTP server
+
+String responseHTML = ""
+                      "<!DOCTYPE html><html><head><title>CaptivePortal</title></head><body>"
+                      "<h1>Hello World!</h1><p>This is a captive portal example. All requests will "
+                      "be redirected here.</p></body></html>";
+
+
+void setup() {
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+  WiFi.softAP("DNSServer CaptivePortal example");
+
+  // if DNSServer is started with "*" for domain name, it will reply with
+  // provided IP to all DNS request
+  dnsServer.start(DNS_PORT, "*", apIP);
+
+  // replay to all requests with same HTML
+  webServer.onNotFound([]() {
+    webServer.send(200, "text/html", responseHTML);
+  });
+  webServer.begin();
+}
+
+void loop() {
+  dnsServer.processNextRequest();
+  webServer.handleClient();
+}
+
+
+

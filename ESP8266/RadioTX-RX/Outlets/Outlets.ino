2d6b1e9cdc971b35b3101f08a3ed20f136fe59b4
metalx1000 <git@filmsbykris.com>
2016-07-13 13:18:24 -0400

    added code to Zap Wall Oulets

---
+++ ESP8266/RadioTX-RX/Outlets/Outlets.ino
@@ -0,0 +1,92 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+const char* ssid = "castle";
+const char* password = "5987cb800f";
+
+ESP8266WebServer server(80);
+
+#include <RCSwitch.h>
+
+RCSwitch mySwitch = RCSwitch();
+
+void handleRoot() {
+  if (server.args() > 0 ) {
+    for ( uint8_t i = 0; i < server.args(); i++ ) {
+      if (server.argName(i) == "switch") {
+        server.send(200, "text/plain", server.arg(i));
+        int code = server.arg(i).toInt();
+        /* Same switch as above, but using decimal code */
+        mySwitch.send(code, 24);
+        delay(1000); 
+        
+      }
+    }
+  }else{
+    server.send(200, "text/plain", "hello from esp8266!");
+  }
+}
+
+void handleNotFound(){
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
+
+void setup(void){
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+
+  server.on("/inline", [](){
+    server.send(200, "text/plain", "this works as well");
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+  // Transmitter is connected to Arduino Pin #13  
+  mySwitch.enableTransmit(13);
+
+  // Optional set pulse length.
+   mySwitch.setPulseLength(185);
+  
+  // Optional set protocol (default is 1, will work for most outlets)
+  // mySwitch.setProtocol(2);
+  
+  // Optional set number of transmission repetitions.
+  // mySwitch.setRepeatTransmit(15);  
+}
+
+void loop(void){
+  server.handleClient();
+}
